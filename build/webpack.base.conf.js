d54b78ddf5edf7e2c3e904ce167556bdc44f0f19
lou <louiscuny@gmail.com>
2018-01-22 11:09:07 +0100

    limit to 60KB base64 files

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -55,7 +55,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: -1,
+          limit: 600000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
@@ -63,7 +63,7 @@ module.exports = {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: -1,
+          limit: 600000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
@@ -71,7 +71,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: -1,
+          limit: 600000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
